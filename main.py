b128a32fad75e130788500333962c758b2e43d9b
Egor Svinarev <egor-rdd@mail.ru>
2022-03-09 17:32:59 +0400

    The calculation of the Fibonacci sequence term starts from 0

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def fib(n):
     if (n < 1):
         return -1
-    elif (n >= 1 and n <= 2):
+    elif n == 1:
+        return 0
+    elif (n >= 2 and n <= 3):
         return 1
     else:
         return fib(n - 1) + fib(n - 2)
