27cf6ea772b1e457d4acb378307a31ffd9120465
Egor Svinarev <egor-rdd@mail.ru>
2022-03-09 17:29:34 +0400

    A function that calculates fibonacci numbers

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello world")
+def fib(n):
+    if (n < 1):
+        return -1
+    elif (n >= 1 and n <= 2):
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+print(fib(10))
